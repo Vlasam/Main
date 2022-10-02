1db39fbfe3141131f0ba0ad3d315d243d31c9ead
Vlasam <vlad.samorukov.99@mail.ru>
2022-10-02 16:33:39 +0300

    Hom1

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿//Урок 1. Знакомство с языком программирования С#
+
+
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+/*Console.WriteLine("Введите число A");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numB = Convert.ToInt32(Console.ReadLine());
+int max = numA;
+if (max > numA )
+{
+    Console.WriteLine("max = A");
+}
+else
+{
+    Console.WriteLine("max = B");
+}
+*/
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*Console.WriteLine("Введите число A");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C");
+int numC = Convert.ToInt32(Console.ReadLine());
+int max = numA;
+if (numA > max ) max = numA;
+if (numB > max) max = numB;
+if (numC > max ) max = numC;
+
+Console.Write("max = ");
+Console.WriteLine(max);*/
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.WriteLine("Введите число");
+double number = Convert.ToDouble(Console.ReadLine());
+
+if ( number % 2 ==0 )
+{
+    Console.WriteLine("Чётное число");
+}
+else
+{
+    Console.WriteLine("Нечётное число");
+}
+*/
+
+
+
