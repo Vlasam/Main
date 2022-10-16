b6f70f81312b80487c4a700f85b45a6d43599eae
Vlasam <108427204+Vlasam@users.noreply.github.com>
2022-10-16 13:00:46 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,53 +1,73 @@
-﻿//Урок 1. Знакомство с языком программирования С#
+//Задача 21
 
-
-//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
-
-/*Console.WriteLine("Введите число A");
-int numA = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число B");
-int numB = Convert.ToInt32(Console.ReadLine());
-int max = numA;
-if (max > numA )
-{
-    Console.WriteLine("max = A");
-}
-else
-{
-    Console.WriteLine("max = B");
-}
-*/
-
-//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-
-/*Console.WriteLine("Введите число A");
-int numA = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число B");
-int numB = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число C");
-int numC = Convert.ToInt32(Console.ReadLine());
-int max = numA;
-if (numA > max ) max = numA;
-if (numB > max) max = numB;
-if (numC > max ) max = numC;
-
-Console.Write("max = ");
-Console.WriteLine(max);*/
-
-//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 /*
-Console.WriteLine("Введите число");
-double number = Convert.ToDouble(Console.ReadLine());
-
-if ( number % 2 ==0 )
+double FindDistans(double x, double y, double e, double a, double b, double c)
 {
-    Console.WriteLine("Чётное число");
+    double num = 0;
+    num = (x - a) * (x - a) + (y - b) * (y - b) + (e - c) * (e - c);
+    num = Math.Sqrt(num);
+    return num;
+}
+
+Console.Write("Введите координату x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату e: ");
+double e = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату b: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату c: ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+double findDistans = FindDistans(x, y, e, a, b, c);
+Console.WriteLine(findDistans);
+*/
+
+//Задача 23
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+/*
+void ShowNumbers(int num)
+{
+    int count = 1;
+    while(count <= num)
+    {
+        double n = Math.Pow(count, 3);
+        Console.Write(n + " ");
+        count++;
+    }
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ShowNumbers(number);
+*/
+
+ //Задача 19
+
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+/*
+Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+int A = num.Length;
+
+if (A == 5)
+{
+    if (num[0] == num[4] && num[1] == num[3])
+    {
+        Console.WriteLine($"{num} - Является");
+    }
+    else
+    {
+        Console.WriteLine($"{num} - Не является");
+    }
 }
 else
 {
-    Console.WriteLine("Нечётное число");
+    Console.WriteLine($"ОШИБКА: {num} - не является пятизначным");
 }
 */
-
-
 
